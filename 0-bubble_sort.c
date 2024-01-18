83ed477de2bfc164ba588c5f08bcfddd3a339293
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-01-18 22:05:46 +0200

    sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,32 +1,33 @@
 #include "sort.h"
+
 /**
-*bubble_sort - using it to store an array of integers
-*
-*@array: pointer to the array
-*
-*@size: the size of array
-*/
+ * bubble_sort - using it to store an array of integers
+ * @array: pointer to the array
+ * @size: the size of array
+ */
 void bubble_sort(int *array, size_t size)
 {
-size_t idx, i;
-int r, swap;
+	size_t idx, i;
+	int r, swap;
+
 	if (array == NULL || size < 2)
 		return;
+
 	for (idx = 0; idx < size - 1; idx++)
-{
+	{
 		swap = 0;
 		for (i = 0; i < size - idx - 1; i++)
-{
+		{
 			if (array[i] > array[i + 1])
-{
+			{
 				r = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = r;
 				swap = 1;
 				print_array(array, size);
-}
-}
+			}
+		}
 		if (swap == 0)
-		break;
-}
+			break;
+	}
 }
