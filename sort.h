1c757e81d1f059d95cc875a04a3bebcf00b8068c
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-01-18 23:49:52 +0200

    sorting

--- sort.h
+++ sort.h
@@ -33,4 +33,9 @@ void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 
+void swap(int *x, int *z);
+int partition(int *arr, int start, int end, size_t size);
+void quick_sort_recursive(int *arr, int start, int end, size_t size);
+void quick_sort(int *array, size_t size);
+
 #endif
